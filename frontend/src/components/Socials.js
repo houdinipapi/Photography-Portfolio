4dd41eb76c1a6f05b88c808ebdd5b5e907224911
Houdini Papi <ochiengcliff.co@gmail.com>
2024-06-05 20:14:32 +0300

    chore: Update header navigation styling for larger screens

--- frontend/src/components/Socials.js
+++ frontend/src/components/Socials.js
@@ -1,9 +1,49 @@
 import React from 'react'
 
+// Import icons
+import {
+  ImYoutube,
+  ImInstagram,
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+} from "react-icons/im"
+
 const Socials = () => {
   return (
     <div className='hidden xl:flex ml-24'>
-      Socials
+      <ul className='flex gap-x-4'>
+        <li>
+          <a href="https://www.facebook.com" target='_blank' rel="noreferrer">
+            <ImFacebook />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.twitter.com" target='_blank' rel="noreferrer">
+            <ImTwitter />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.instagram.com" target='_blank' rel="noreferrer">
+            <ImInstagram />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.pinterest.com" target='_blank' rel="noreferrer">
+            <ImPinterest />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.youtube.com" target='_blank' rel="noreferrer">
+            <ImYoutube />
+          </a>
+        </li>
+
+      </ul>
     </div>
   )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import HomeImage from "../img/home/woman.png";
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <section className='section bg-pink-200'>Home</section>
+    <section className='section'>
+      <div className='container mx-auto h-full relative'>
+        {/* Text and Image Wrapper */}
+        <div className='flex flex-col justify-center'>
+
+          {/* Text */}
+          <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb:0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+            <h1 className='h1'>
+              Photographer <br /> & Editor
+            </h1>
+            <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>Nairobi, Kenya</p>
+            <Link to={"/contact"} className='btn mb-[30px] rounded-2xl'>Hire Me</Link>
+          </div>
+
+          {/* Image */}
+          <div className='flex justify-end max-h-96 lg:max-h-max'>
+            <div className='relative lg:-right-40 overflow-hidden'>
+              <img
+                src={HomeImage}
+                alt='Home'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
