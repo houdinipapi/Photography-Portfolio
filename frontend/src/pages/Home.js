f66707cf525aad302347c7b7ed1aa62a0414702c
Houdini Papi <ochiengcliff.co@gmail.com>
2024-06-06 14:28:58 +0300

    chore: Add lazy loading for images in Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,35 +1,55 @@
 import React from 'react'
 import HomeImage from "../img/home/woman.png";
 import { Link } from "react-router-dom"
+import { motion } from "framer-motion"
+import { transition1 } from "../transitions"
 
 const Home = () => {
   return (
-    <section className='section bg-pink-200'>
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={transition1}
+      className='section bg-pink-200'
+    >
       <div className='container mx-auto h-full relative'>
         {/* Text and Image Wrapper */}
         <div className='flex flex-col justify-center'>
 
           {/* Text */}
-          <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb:0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'>
+          <motion.div
+            initial={{ opacity: 0, y: "-50%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-50%" }}
+            transition={transition1}
+            className='w-full pt-36 pb-14 lg:pt-0 lg:pb:0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'
+          >
             <h1 className='h1'>
               Photographer <br /> & Editor
             </h1>
             <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>Nairobi, Kenya</p>
             <Link to={"/contact"} className='btn mb-[30px] rounded-2xl'>Hire Me</Link>
-          </div>
+          </motion.div>
 
           {/* Image */}
           <div className='flex justify-end max-h-96 lg:max-h-max'>
-            <div className='relative lg:-right-40 overflow-clipped'>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={transition1}
+              className='relative lg:-right-40 overflow-clipped'
+            >
               <img
                 src={HomeImage}
                 alt='Home'
               />
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   )
 }
 
